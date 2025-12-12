8897b03d6f3119205de6acd8b7f654ec09ba052e
Nadeen-X5 <nyalameer2016@gmail.com>
2025-12-12 13:33:50 +0300

    Modify DEPARTMENT table and EMPLOYEE  and  insert data into all tables

--- COMPANY.sql
+++ COMPANY.sql
@@ -1,11 +1,17 @@
+-------------------------------------------------------
+--              CREATE ALL TABLES 
+-------------------------------------------------------
+
 -- Creating DEPARTMENT Table.
 CREATE TABLE DEPARTMENT(
 D_number INTEGER   NOT NULL,
-D_name VARCHAR(15) NOT NULL,
+D_name VARCHAR(30) NOT NULL,
 D_description VARCHAR(200) ,
 PRIMARY KEY(D_number),
 UNIQUE(D_name)
 );
+
+
 -- Creating EMPLOYEE Table.
 CREATE TABLE EMPLOYEE (
 E_id INTEGER     NOT NULL,
@@ -90,18 +96,26 @@ CREATE TABLE TICKET_LINKED_TO_KBA (
     CONSTRAINT fk_link_ticket FOREIGN KEY (T_id) REFERENCES TICKET(T_id) ON DELETE CASCADE,
     CONSTRAINT fk_link_kba FOREIGN KEY (KBA_id) REFERENCES KNOWLEDGE_BASE_ARTICLE(KBA_id) ON DELETE CASCADE
 );
+
+-------------------------------------------------------
+--      INSERT INITIAL DATA INTO ALL TABLES
+-------------------------------------------------------
+
+-- 1. INSERT DATA INTO DEPARTMENT TABLE
 INSERT INTO DEPARTMENT VALUES (1, 'IT', 'Handles technical and help desk support');
 INSERT INTO DEPARTMENT VALUES (2, 'HR', 'Human resources and employee relations');
 INSERT INTO DEPARTMENT VALUES (3, 'FINANCE', 'Financial and accounting department');
 INSERT INTO DEPARTMENT VALUES (4, 'Marketing', 'Marketing and communication');
 INSERT INTO DEPARTMENT VALUES (5, 'Risk Management', 'Identifying and mitigating organizational risks');
-INSERT INTO DEPARTMENT VALUES (6, 'Quality Assurance', 'Ensures process quality, standards compliance, and continuous improvement within the organization');
+INSERT INTO DEPARTMENT VALUES (6, 'Quality Assurance',
+'Ensures process quality, standards compliance, and continuous improvement within the organization');
 
+-- 2. INSERT DATA INTO EMPLOYEE TABLE
 INSERT INTO EMPLOYEE VALUES (100, 'Reham',   'Mobarak',  'rere@icloud.com',   '0502000001', 1);
 INSERT INTO EMPLOYEE VALUES (101, 'Nadeen',  'yahya',  'nadeen@gmail.com',  '0500000002', 1);
 INSERT INTO EMPLOYEE VALUES (102, 'Mohamad',  'Fahad', 'mmm@hotmail.com',  '0500000003', 2);
 INSERT INTO EMPLOYEE VALUES (103, 'Leen',  'Khaled', 'lno@icloud.com',  '0500000004', 5);
-INSERT INTO EMPLOYEE VALUES (104, 'Ahmad',  'Yousef', 'Ahmad22@gmail.com',  '0500000005', 3);
+INSERT INTO EMPLOYEE VALUES (104, 'Ahmad',  'Yousef', 'Ahmad22@gmail.com',  '05000010005', 3);
 INSERT INTO EMPLOYEE VALUES (105, 'Fahad', 'Khalid', 'fahad@gmail.com', '0500000006', 6);
 INSERT INTO EMPLOYEE VALUES (106, 'Ali',   'Saleh',  'ali@gmail.com',   '0500000001', 4);
 INSERT INTO EMPLOYEE VALUES (107, 'Sara',  'Ahmed',  'sara@icloud.com',  '0500001112', 1);
@@ -110,6 +124,7 @@ INSERT INTO EMPLOYEE VALUES (109, 'Lama',  'Hassan', 'lama@outlook.com',  '05000
 INSERT INTO EMPLOYEE VALUES (110, 'Reem',  'Yousef', 'reem@icloud.com',  '0500001115', 4);
 INSERT INTO EMPLOYEE VALUES (111, 'Sama', 'Ali', 'soso@gmail.com', '0500001116', 6);
 
+-- 3. INSERT DATA INTO CUSTOMER TABLE
 INSERT INTO CUSTOMER VALUES (111, 'HQ');
 INSERT INTO CUSTOMER VALUES (102, 'Riyadh Branch');
 INSERT INTO CUSTOMER VALUES (103, 'Dammam Branch');
@@ -117,6 +132,7 @@ INSERT INTO CUSTOMER VALUES (104, 'Jeddah Branch');
 INSERT INTO CUSTOMER VALUES (105, 'HQ');
 INSERT INTO CUSTOMER VALUES (107, 'Riyadh Branch');
 
+-- 4. INSERT DATA INTO SUPPORT_AGENT TABLE
 INSERT INTO SUPPORT_AGENT VALUES (100, 'Support Engineer');
 INSERT INTO SUPPORT_AGENT VALUES (101, 'Team Leader');
 INSERT INTO SUPPORT_AGENT VALUES (106, 'Senior Agent');
@@ -124,9 +140,47 @@ INSERT INTO SUPPORT_AGENT VALUES (108, 'Support Engineer');
 INSERT INTO SUPPORT_AGENT VALUES (109, 'Junior Agent');
 INSERT INTO SUPPORT_AGENT VALUES (110, 'Senior Agent');
 
+-- 5. INSERT DATA INTO SA_SKILLS TABLE
 INSERT INTO SA_SKILLS VALUES (100, 'Network Troubleshooting');
 INSERT INTO SA_SKILLS VALUES (100, 'Email Support');
 INSERT INTO SA_SKILLS VALUES (101, 'Hardware Repair');
 INSERT INTO SA_SKILLS VALUES (106, 'System Administration');
 INSERT INTO SA_SKILLS VALUES (101, 'Database Support');
 INSERT INTO SA_SKILLS VALUES (110, 'VPN Configuration');
+
+-- 6. INSERT DATA INTO TICKET TABLE
+INSERT INTO TICKET VALUES (1001, 'Login Issue', 'I cannot login to email', 'Open', 'High', TO_DATE('2025-11-20','YYYY-MM-DD'), NULL, 111, 100, 1);
+INSERT INTO TICKET VALUES (1002, 'Email not working', 'I cannot send or receive emails', 'Closed', 'Medium', TO_DATE('2025-11-15','YYYY-MM-DD'), TO_DATE('2025-11-16','YYYY-MM-DD'), 101, 100, 1);
+INSERT INTO TICKET VALUES (1003, 'Slow Wifi', 'The internet keeps disconnecting on me', 'In Progress', 'Medium', TO_DATE('2025-11-21','YYYY-MM-DD'), NULL, 103, 101, 1);
+INSERT INTO TICKET VALUES (1004, 'System performance issue', 'The computer is running very slow', 'Open', 'Critical', TO_DATE('2025-11-22','YYYY-MM-DD'), NULL, 105, 106, 1);
+INSERT INTO TICKET VALUES (1005, 'Software Install Issue', 'I need Oracle to be installed', 'Closed', 'Low', TO_DATE('2025-11-10','YYYY-MM-DD'), TO_DATE('2025-11-11','YYYY-MM-DD'), 105, 108, 1);
+INSERT INTO TICKET VALUES (1006, 'Forgot password request', 'I need to reset my password', 'Open', 'Low', TO_DATE('2025-11-22','YYYY-MM-DD'), NULL, 110, 100, 1);
+
+-- 7. INSERT DATA INTO ATTACHMENT TABLE
+INSERT INTO ATTACHMENT VALUES ('screenshot_login_error.png', 1001, 'PNG', TO_DATE('2025-11-20','YYYY-MM-DD'));
+INSERT INTO ATTACHMENT VALUES ('screenshot_email_not_working.png', 1002, 'PNG', TO_DATE('2025-11-15','YYYY-MM-DD'));
+INSERT INTO ATTACHMENT VALUES ('Wifi_error_messages.txt', 1003, 'txt', TO_DATE('2025-11-21','YYYY-MM-DD'));
+INSERT INTO ATTACHMENT VALUES ('system_report.pdf', 1004, 'PDF', TO_DATE('2025-11-22','YYYY-MM-DD'));
+INSERT INTO ATTACHMENT VALUES ('screenshot_install_error.png', 1005, 'PNG', TO_DATE('2025-11-10','YYYY-MM-DD'));
+INSERT INTO ATTACHMENT VALUES ('screenshot_password_error.jpg', 1006, 'JPG', TO_DATE('2025-11-22','YYYY-MM-DD'));
+
+-- 8. INSERT DATA INTO KNOWLEDGE_BASE_ARTICLE TABLE
+INSERT INTO KNOWLEDGE_BASE_ARTICLE VALUES (500, 'Email Login', 'Check password, server status, and network connection.', 'Software', TO_DATE('2025-05-01','YYYY-MM-DD'), 106);
+INSERT INTO KNOWLEDGE_BASE_ARTICLE VALUES (501, 'Fix Email', 'Check email server status, verify login credentials, and clear email application cache.', 'Hardware', TO_DATE('2025-01-15','YYYY-MM-DD'), 100);
+INSERT INTO KNOWLEDGE_BASE_ARTICLE VALUES (502, 'Wifi Troubleshooting', 'Check router power and connections, restart device, and verify correct network password.', 'Network', TO_DATE('2025-02-01','YYYY-MM-DD'), 108);
+INSERT INTO KNOWLEDGE_BASE_ARTICLE VALUES (503, 'Oracle Install Guide', 'Run setup.exe', 'Software', TO_DATE('2025-03-01','YYYY-MM-DD'), 101);
+INSERT INTO KNOWLEDGE_BASE_ARTICLE VALUES (504, 'Common System Performance Optimization', 'Try to: 1. Restart computer 2.Close unnecessary applications 3. Clear browser cache 4.Check for Windows/Mac updates', 'Network', TO_DATE('2025-04-01','YYYY-MM-DD'), 109);
+INSERT INTO KNOWLEDGE_BASE_ARTICLE VALUES (505, 'Reset Password', 'Steps to Reset Your Password: 1.Visit the Login Page: Go to the login page of your email account. 2.Click on "Forgot Password"and change your password' , 'Security', TO_DATE('2025-01-01','YYYY-MM-DD'), 100);
+
+-- 9. INSERT DATA INTO TICKET_LINKED_TO_KBA TABLE
+INSERT INTO TICKET_LINKED_TO_KBA VALUES (1001, 500);
+INSERT INTO TICKET_LINKED_TO_KBA VALUES (1002, 501);
+INSERT INTO TICKET_LINKED_TO_KBA VALUES (1003, 502);
+INSERT INTO TICKET_LINKED_TO_KBA VALUES (1005, 503);
+INSERT INTO TICKET_LINKED_TO_KBA VALUES (1003, 504);
+INSERT INTO TICKET_LINKED_TO_KBA VALUES (1001, 505);
+
+
+
+
+
